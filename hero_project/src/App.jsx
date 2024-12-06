13b9794c4f01e6a30d9d7a411cb12874aa5511c2
aditi saudagar <aditisaudagar17@gmail.com>
2024-12-06 12:10:23 +0530

    hero project

---
+++ hero_project/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Hero from './Hero.jsx'
+
+function App(){
+  return(
+    <>
+    
+    <Hero/>
+    <br/>
+    </>
+  )
+}
+export default App
---
+++ hero_project/src/Hero.jsx
@@ -0,0 +1,12 @@
+import myimg from '/allen cake.jpg'
+
+const Hero = () => {
+return(
+    <>
+    <img src="cake.jpg" class="image"/>
+<br/>
+    <img src={myimg} class="image"/>
+    </>
+)
+}
+export default Hero
